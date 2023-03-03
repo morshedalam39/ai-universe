f87105950491917beb0546839dc09bf70d442c17
morshedalam39 <morshedalam4239@gmail.com>
2023-03-03 23:33:27 +0600

    card functions added

--- js/index.js
+++ js/index.js
@@ -82,7 +82,58 @@ const getSingleData = (id) => {
 
 const showSingleData = (data) => {
   document.getElementById('modalBody').innerHTML=`
- 
+  <div class="w-50 border border-danger-subtle p-3 bg-danger-subtle">
+  <h4 class=" fs-5" id="detailsModalLabel">${data.description}</h4>
+  <div class="d-flex gap-3 mt-3">
+  <div class="border border-secondary-subtle rounded p-2 bg-light text-success">
+  <ul style="list-style:none">
+  <li class="text-center"><h6>${data.pricing[0].price ? data.pricing[0].price : 'Free of Cost/Basic'}</h6></li>
+  <li><h5>${data.pricing[0].plan}</h5></li>
+  </ul>
+  </div>
+  <div class="border border-secondary-subtle rounded p-2 bg-light text-danger">
+  <ul style="list-style:none">
+  <li ><h6>${data.pricing[1].price}</h6></li>
+  <li><h5>${data.pricing[1].plan}</h5></li>
+  </ul>
+  </div>
+  <div class="border border-secondary-subtle rounded p-2 bg-light text-danger-emphasis">
+  <ul style="list-style:none">
+  <li ><h6>${data.pricing[2].price}</h6></li>
+  <li><h5>${data.pricing[2].plan}</h5></li>
+  </ul>
+  </div>
+  </div>
+
+  <div class="d-flex gap-5">
+  <div class="mt-4">
+  <h4>Features</h4>
+  <ul>
+  <li>${data.features[1].feature_name}</li>
+  <li>${data.features[2].feature_name}</li>
+  <li>${data.features[3].feature_name}</li>
+  </ul>
+  </div>
+  <div class="mt-4">
+  <h4>Integrations</h4>
+  <ul>
+  <li>${data.integrations[0] ? data.integrations[0] : 'No Data Found'}</li>
+  <li>${data.integrations[1] ? data.integrations[1] : 'No Data Found'}</li>
+  <li>${data.integrations[2] ? data.integrations[2] : 'No Data Found'}</li>
+  </ul>
+  </div>
+  </div>
+  </div>
+
+  <div class="w-50 border border-danger-subtle p-3">
+  <img class="img-fluid" src="${data.image_link[0]}">
+  <div class="position-relative">
+
+  
+  </div>
+  <h5 class="text-center  mt-2 fw-bold">${data.input_output_examples[0].input}</h5>
+  <p class="text-center mt-2">${data.input_output_examples[1].output ? data.input_output_examples[1].output : 'No! Not Yet! Take a break!'}</p>
+  </div>
   `
 
 };
